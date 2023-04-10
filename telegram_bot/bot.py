89008f1153aa3a4976fa5c56808690a93a183d71
LapEg <laptevegor666@gmail.com>
2023-04-11 01:55:53 +0300

    database was modified, bad working of system of loging in

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -4,12 +4,13 @@ from telegram.ext import Application, MessageHandler, filters, CommandHandler, C
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 import sqlite3
+import hashlib
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
-TURNED_OFF, STARTED, IN_MENU, IN_LOGINING = range(4)
+TURNED_OFF, STARTED, IN_MENU, IN_LOGINING, IN_PASSWORD = range(5)
 
 logger = logging.getLogger(__name__)
 load_dotenv()
@@ -18,6 +19,7 @@ token = os.getenv('token')
 
 class User:
     def __init__(self):
+        self.login = None
         self.is_guest = True
         self.keyboard_pos = 0
         self.keyboards = [
@@ -64,17 +66,7 @@ class User:
         )
         return TURNED_OFF
 
-    async def get_login(self, update, context):
-        # login =
-        # print(login)
-        # res = self.cur.execute(f"""SELECT surname FROM users WHERE email = {login}""")
-        # print(res)
-        await update.message.reply_text(
-            text=self.cur.execute(f"""SELECT surname FROM users WHERE email = '{update.message.text}'""").fetchone()
-        )
-        # return IN_MENU
-
-    async def login(self, update, context):
+    async def check_login(self, update, context):
         print(2)
         self.keyboard_pos = 3
         self.reply_keyboard = self.keyboards[self.keyboard_pos]
@@ -83,8 +75,40 @@ class User:
             text="Введите логин",
             reply_markup=markup
         )
+        print(1)
+        self.login = update.message.text
+        self.cur.execute(f"SELECT user_id FROM users WHERE login = '{self.login}'")
+        if self.cur.fetchone() is None:
+            await update.message.reply_text(
+                text="Аккаунта не существует",
+                reply_markup=markup
+            )
+            return IN_LOGINING
+        else:
+            await update.message.reply_text(
+                text="Логин введен верно\nДалее введите пароль",
+                reply_markup=markup
+            )
+            return IN_PASSWORD
 
-        return IN_LOGINING
+    async def check_password(self, update, context):
+        markup = ReplyKeyboardMarkup(self.reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            text="Введите пароль",
+            reply_markup=markup
+        )
+        hashed_password = hashlib.md5(update.message.text)
+        if self.cur.execute(f"""SELECT password FROM users WHERE login = {self.login}""").fetchone() == hashed_password:
+            await update.message.reply_text(
+                text=f"Вы вошли в систему под логином {self.login}",
+            )
+            self.is_guest = False
+            return IN_MENU
+        else:
+            await update.message.reply_text(
+                text="Неверный пароль, попробуйте снова"
+            )
+            return IN_PASSWORD
 
     async def back(self, update, context):
         self.keyboard_pos -= 1
@@ -97,9 +121,7 @@ class User:
     def main(self):
         application = Application.builder().token(token).build()
         conv_handler = ConversationHandler(
-            # точка входа в разговор
             entry_points=[CommandHandler('start', self.start)],
-            # словарь состояний разговора, возвращаемых callback функциями
             states={
                 STARTED: [
                     MessageHandler(
@@ -112,7 +134,7 @@ class User:
                 # Этап `SECOND` - происходит то же самое, что и в описании этапа `FIRST`
                 IN_MENU: [
                     # MessageHandler(filters.Regex("Registration"), self.reg),
-                    MessageHandler(filters.Regex("Log in"), self.login),
+                    MessageHandler(filters.Regex("Log in"), self.check_login),
                     # MessageHandler(filters.Regex("My profile"), self.profile),
                     # MessageHandler(filters.Regex("Log out"), self.logout),
                     # MessageHandler(filters.Regex("Chat GPT's stories"), self.gpt),
@@ -122,7 +144,10 @@ class User:
                     MessageHandler(filters.Regex("Start"), self.start)
                 ],
                 IN_LOGINING: [
-                    MessageHandler(filters.TEXT, self.get_login)
+                    MessageHandler(filters.TEXT, self.check_login)
+                ],
+                IN_PASSWORD: [
+                    MessageHandler(filters.TEXT, self.check_password)
                 ]
             },
             # точка выхода из разговора
